36818a3e15bb974b076b9eb66c3ffc7776f8c86a
Tobias van Driessel <tobiasvandriessel@startmail.com>
2021-10-10 12:05:02 +0200

    Change build script to write to convenient location for CMake, and introduce CMakeLists.txt for building with CMake.

--- build.rs
+++ build.rs
@@ -1,14 +1,34 @@
 extern crate cbindgen;
 
 use std::env;
+use std::path::PathBuf;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    let package_name = env::var("CARGO_PKG_NAME").unwrap();
+
+    let output_file = target_dir()
+        .join(format!("{}.h", package_name))
+        .display()
+        .to_string();
 
     cbindgen::Builder::new()
       .with_crate(crate_dir)
       .with_language(cbindgen::Language::Cxx)
       .generate()
       .expect("Unable to generate bindings")
-      .write_to_file("problem_generator.h");
+      .write_to_file(&output_file);
+}
+
+/// From https://michael-f-bryan.github.io/rust-ffi-guide/cbindgen.html : 
+/// Find the location of the `target/` directory. Note that this may be 
+/// overridden by `cmake`, so we also need to check the `CARGO_TARGET_DIR` 
+/// variable.
+fn target_dir() -> PathBuf {
+  if let Ok(target) = env::var("CARGO_TARGET_DIR") {
+      PathBuf::from(target)
+  } else {
+      PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("target")
+  }
 }
