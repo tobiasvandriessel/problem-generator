f273121ba1b56585e1fe8e555c2528dc29f9d614
Tobias van Driessel <tobiasvandriessel@startmail.com>
2022-01-01 21:08:49 +0100

    Add ChaChaRng struct manually to header file

--- build.rs
+++ build.rs
@@ -16,6 +16,7 @@ fn main() {
     cbindgen::Builder::new()
       .with_crate(crate_dir)
       .with_language(cbindgen::Language::Cxx)
+      .with_after_include("\nstruct ChaChaRng;")
       .generate()
       .expect("Unable to generate bindings")
       .write_to_file(&output_file);
